b46fd5d538b7b839d3aaf648870dad43d4d14c7b
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2020-03-09 10:30:12 +0530

    Added test configs

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,13 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
   });
 };
